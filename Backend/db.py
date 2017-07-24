eb6039459467d8240f0a465c9aaf04a6beb2f2d2
Jonas Garcia <jonasgarcia666@gmail.com>
2017-07-24 16:46:10 -0300

    Ajustes no layout de provedor

--- Backend/db.py
+++ Backend/db.py
@@ -4,28 +4,27 @@ from whois import genericWhois
 import json
 import re
 
-
-
 #Função que verifica se os dados de whois já constam no banco, caso não constem eles são adicionados.
 def verify(url):
     #conexao com o mongolab
     connection = MongoClient("ds155651.mlab.com", 55651)
     db = connection["whois"]
     db.authenticate("whoisbot", "whoisbot10")
+    url  = re.findall(r"^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)", url)[0]
     #select para encontrar se a url já foi inserida no banco
     data = db.data.find_one({"url":str(url)})
     #verifica se já esta no banco, caso contrario realiza a pesquisa
     if data is not None:
-        return data[u'data']
+        return {'url':url,'data':data[u'data'], 'host':data[u'host']}
     else:
         #chama a função que retorna o servidor de whois
         host = getWhois(url)
         #chama a função que retorna os dados do whois e arruma os problemas de encoding.
-        msg = genericWhois(host,url).decode('latin-1')
+        msg = genericWhois(host,url)
         #chama a função que salva os dados buscados no banco
         save(url,msg)
         #retorna os dados pesquisados
-        return msg
+        return {'url':url,'data':msg[0].decode('latin-1').encode('utf-8'), 'host':findHost(msg[1].encode('utf-8'))}
 
 #Função que recebe a url e retorna o servidor de whois
 def getWhois(url):
@@ -53,7 +52,19 @@ def save(url,data):
     db = connection["whois"]
     db.authenticate("whoisbot", "whoisbot10")
     #insert no banco lidando com o problema de encode
-    db.data.insert_one({'url':url,'data':data.encode('utf-8')})
+    db.data.insert_one({'url':url,'data':data[0].decode('latin-1').encode('utf-8'), 'host':findHost(data[1])})
     connection.close()
+
+#De/Para provedores
+def findHost(host):
+    if 'aws' in host.lower():
+        return "Amazon AWS"
+    if 'godaddy' in host.lower():
+        return "Go Daddy"
+    else:
+        return host.title()
+
+
+
 
 
--- Backend/whois.py
+++ Backend/whois.py
@@ -1,4 +1,5 @@
 import socket
+import re
 #Chamada generica que ira trazer o dados do whois
 def genericWhois(server,query):
     #Conexao com o socket
@@ -9,19 +10,33 @@ def genericWhois(server,query):
      
     #Tratatamento da reposta
     msg = ''
+    host = ''
+    owner=''
     while len(msg) < 10000:
         chunk = s.recv(100)
         if(chunk == ''):
             break
         msg = msg + chunk
     
-#         #Now scan the reply for the whois server
-#         lines = msg.splitlines()
-#         for line in lines:
-#             if ':' in line:
-#                 words = line.split(':')
-#                 if  'Whois' in words[0] and 'whois.' in words[1]:
-#                     whois = words[1].strip()
-#                     break;
+        #Now scan the reply for the whois server
+        lines = msg.splitlines()
+        for line in lines:
+            if ':' in line:
+                words = line.split(':')
+                if 'nserver' in words[0] and host == '':
+                    if len(re.findall(r"\.(.*?)(?:\.a*)", words[1].strip())) >= 1:
+                        host = re.findall(r"\.(.*?)(?:\.a*)", words[1].strip())[0]
+                        break
+                if 'Name Server' in words[0] and host == '':
+                    if len(re.findall(r"\.(.*?)(?:\.a*)",words[1].strip())) >= 1:
+                        host = re.findall(r"\.(.*?)(?:\.a*)", words[1].strip())[0]
+                        break
+                    if len(re.findall(r"(?:\.)[a-zA-Z]*",words[1].strip())) >= 1:
+                        host = re.findall(r"(?:\.)[a-zA-Z]*", words[1].strip())[0].replace('.','')
+                        break
+             
     # Retorna a resposta tratada
-    return msg
+    return [msg,host,owner]
+
+# a =  genericWhois('whois.registro.br','stackoverflow.com')
+# print a
